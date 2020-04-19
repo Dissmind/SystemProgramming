15a001f64b5f065914fbedc5d2aa77029e22fce5
RossiVienas <Dissmind@gmail.com>
2020-04-19 07:19:28 +0200

    Add project files.

---
+++ SystemProgramming/MenuFirstLabs.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SystemProgramming
+{
+    /// <summary>
+    /// Interaction logic for MenuFirstLabs.xaml
+    /// </summary>
+    public partial class MenuFirstLabs : Window
+    {
+        public MenuFirstLabs()
+        {
+            InitializeComponent();
+        }
+    }
+}
