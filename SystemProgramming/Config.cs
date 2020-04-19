8e4b5a49dba641834faf9d295819e8c4358496ae
RossiVienas <Dissmind@gmail.com>
2020-04-19 07:34:40 +0200

    [ADD]: init main menu, init config

---
+++ SystemProgramming/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemProgramming
+{
+    class Config
+    {
+        public static int Variant { get; set; } = 1;
+    }
+}
