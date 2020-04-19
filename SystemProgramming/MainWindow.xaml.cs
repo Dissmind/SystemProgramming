a2b8585812f2af51ebe22e72d11e9960c2d5bebc
RossiVienas <Dissmind@gmail.com>
2020-04-19 08:25:38 +0200

    [ADD]: variant combox

--- SystemProgramming/MainWindow.xaml.cs
+++ SystemProgramming/MainWindow.xaml.cs
@@ -24,5 +24,31 @@ namespace SystemProgramming
         {
             InitializeComponent();
         }
+
+        private void Open_Menu_First_Labs(object sender, RoutedEventArgs e)
+        {
+            var win = new MenuFirstLabs();
+            win.Show();
+        }
+
+        private void Open_Menu_Second_Labs(object sender, RoutedEventArgs e)
+        {
+            var win = new MenuSecondLabs();
+            win.Show();
+        }
+
+        private void Variant_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = (ComboBox)sender;
+            string selectedItem = Variant_ComboBox.SelectedItem.ToString()
+                .Split(new string[] { ": " }, StringSplitOptions.None).Last();
+
+            // TODO: FIX this later
+            try
+            {
+                Config.Variant = int.Parse(selectedItem);
+            } 
+            catch { }
+        }
     }
 }
